d023dac61d785a86b98249cbec18ac173b134330
Aleksander Gajewski <adiog@brainfuck.pl>
2021-10-16 07:22:54 +0200

    FisCher

--- arcade/getapk.js
+++ arcade/getapk.js
@@ -1,7 +1,7 @@
 function getapk() {
   if (confirm("The App does not require any permissions.\n" +
               "It works entirely offline.\n" +
-              "The original sha1sum of file: 1a1f928bb7d1b144c72818e501aae41f41fd7929.\n" +
+              "The original sha1sum of file: e5e66e797ca741770e85562e39b4da6dae356ffd.\n" +
               "If you encounter any suspicious App behaviour please contact wychess@wychess.com.\n" +
               "Please confirm that you understand the risk of installing software from malicious sources.")) {
     document.location = 'https://wychess.github.io/release/wychess-arcade-v1.14rc.apk'
