7281e6417cf289a30ae2d9e8a60b76ecbe051dab
Aleksander Gajewski <adiog@brainfuck.pl>
2021-10-24 06:32:53 +0200

    Clear Logs within App

--- arcade/getapk.js
+++ arcade/getapk.js
@@ -1,9 +1,9 @@
 function getapk() {
   if (confirm("The App does not require any permissions.\n" +
               "It works entirely offline.\n" +
-              "The original sha1sum of file: 3abc26a825789ff1e11a6569bdc83c500432f406.\n" +
+              "The original sha1sum of file: d3b2358fb73304d84d6db548438af0cdf621c112.\n" +
               "If you encounter any suspicious App behaviour please contact wychess@wychess.com.\n" +
               "Please confirm that you understand the risk of installing software from malicious sources.")) {
-    document.location = 'https://wychess.github.io/release/wychess-arcade-v1.16rc.apk'
+    document.location = 'https://wychess.github.io/release/wychess-arcade-v1.17rc.apk'
   }
 }
